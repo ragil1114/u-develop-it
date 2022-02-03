2804ea8cae81954f4cf26bc752c721f68fe950fd
Ragil14 <rahil.gilani@gmail.com>
2022-02-03 17:50:31 -0600

    created API Endpoint to create a candidate

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-// Import MySQL2 module
+// import MySQL2 module
 const mysql = require('mysql2');
+// import statement to allow use of inputCheck() function
+const inputCheck = require('./utils/inputCheck');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -75,6 +77,32 @@ app.delete('/api/candidate/:id', (req, res) => {
           id: req.params.id
         });
       }
+    });
+});
+
+// Create a candidate
+app.post('/api/candidate', ({ body }, res) => {
+    // inputCheck() Function/Module to verify that user info in the request can create a candidate
+    // to use this function, we must import the module at the top of this file
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+        VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
     });
 });
 
