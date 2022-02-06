63123843dfd06c1a71fe054d05a49b31cc32ca24
Ragil14 <rahil.gilani@gmail.com>
2022-02-06 17:40:35 -0600

    added Constraints to votes table

--- db/schema.sql
+++ db/schema.sql
@@ -32,4 +32,7 @@ CREATE TABLE votes (
   voter_id INTEGER NOT NULL,
   candidate_id INTEGER NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
