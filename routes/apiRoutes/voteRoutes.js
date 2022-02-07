cc99af2ba38a08d52c1c7ceb434107ccb7273ab9
Ragil14 <rahil.gilani@gmail.com>
2022-02-06 18:31:37 -0600

    added GET Route for total votes for all candidates

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -3,6 +3,29 @@ const router = express.Router();
 const db = require('../../db/connection');
 const inputCheck = require('../../utils/inputCheck');
 
+// Get the total votes for all the candidates
+router.get('/votes', (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, 
+                  COUNT(candidate_id) 
+                  AS count FROM votes 
+                  LEFT JOIN candidates ON votes.candidate_id = candidates.id 
+                  LEFT JOIN parties ON candidates.party_id = parties.id 
+                  GROUP BY candidate_id 
+                  ORDER BY count DESC`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+});
+
+// Create a vote record
 router.post('/vote', ({ body }, res) => {
     // Data validation
     const errors = inputCheck(body, 'voter_id', 'candidate_id');
